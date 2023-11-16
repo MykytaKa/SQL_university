b8898d40f997be97d013477857d51b79b56966da
MykytaKa <128030287+MykytaKa@users.noreply.github.com>
2023-11-16 14:28:32 +0200

    create tabel connections

--- lab6/lab.sql
+++ lab6/lab.sql
@@ -8,43 +8,28 @@ WHERE cities.region = regions.uuid AND regions.name = "Nord";
 
 DROP TABLE IF EXISTS lines;
 CREATE TABLE lines (
-  id int(256) NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  color varchar(255) NOT NULL,
-  start_station_id int(256) NOT NULL,
-  end_station_id int(256) NOT NULL,
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  color VARCHAR(255) NOT NULL,
+  start_station_id INT(10) NOT NULL,
+  end_station_id INT(10) NOT NULL,
   PRIMARY KEY (line_id)
 ) 
 
-
-DROP TABLE IF EXISTS schedule;
-CREATE TABLE schedule (
-  id int(256) unsigned NOT NULL AUTO_INCREMENT,
-  stop_id int(256) NOT NULL,
-  day_of_week varchar(20) NOT NULL,
-  departure_time time NOT NULL,
-  PRIMARY KEY (schedule_id)
-) 
-
-
 DROP TABLE IF EXISTS stations;
 CREATE TABLE stations (
-  id int(256) NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  latitude decimal(10,6) NOT NULL,
-  longitude decimal(10,6) NOT NULL,
-  next_station_id int(256) DEFAULT NULL,
-  prev_station_id int(256) DEFAULT NULL,
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  latitude DECIMAL(10,6) NOT NULL,
+  longitude DECIMAL(10,6) NOT NULL,
+  next_station_id INT(10) DEFAULT NULL,
+  prev_station_id INT(10) DEFAULT NULL,
   PRIMARY KEY (id)
 ) 
 
-
-DROP TABLE IF EXISTS stops;
-CREATE TABLE stops (
-  id int(256) unsigned NOT NULL AUTO_INCREMENT,
-  station_id int(256) NOT NULL,
-  line_id int(256) NOT NULL,
-  stop_sequence int(256) NOT NULL,
-  arrival_time time NOT NULL,
-  PRIMARY KEY (stop_id)
-) 
+DROP TABLE IF EXISTS connections;
+CREATE TABLE connections (
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  first_station_id INT(10) NOT NULL,
+  second_station_id INT(10) NOT NULL,
+  PRIMARY KEY (id)
